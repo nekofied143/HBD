d2b9023eab6e24b837f6a14ab7e5256bc1b3103e
nekofied143 <112548898+nekofied143@users.noreply.github.com>
2026-07-29 11:05:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,10 @@ async function loadCelebrants() {
 
     try {
 
-        const response = await fetch("celebrants.json");
+        const response =
+           await fetch(
+              "https://script.google.com/macros/s/AKfycbwWtmu9j_umIJG8CYvfgdofzTXSvSlOymlFjD2hf6PoRgBih6Jzn_JUYOSksNe_8QnH/exec"
+              );
 
         if (!response.ok) {
             throw new Error("Unable to load celebrants.json");
